e4d1c26306336da3a7398362750bcb2a44f2a4f1
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-11-19 00:11:47 -0200

    add custom error

---
+++ src/errors/AcknowledgeableError.ts
@@ -0,0 +1,23 @@
+import { Robot, Response } from 'hubot'
+import SlackAdapter from 'hubot-slack'
+
+/**
+ * An error message that should submit a message
+ *  answering an invalid call or something like that.
+ * The idea is that the error will be submitted as a message
+ *  on the channel that originated the error.
+ */
+export class AcknowledgeableError extends Error {
+  robot: Robot<SlackAdapter>
+  res: Response<SlackAdapter>
+
+  constructor(
+    message: string,
+    robot: Robot<SlackAdapter>,
+    res: Response<SlackAdapter>,
+  ) {
+    super(message)
+    this.robot = robot
+    this.res = res
+  }
+}
