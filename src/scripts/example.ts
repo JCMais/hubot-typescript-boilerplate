b3abcf129acbd0900617726c55d988b369276333
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-11-19 00:13:16 -0200

    improve example script

--- src/scripts/example.ts
+++ src/scripts/example.ts
@@ -16,16 +16,41 @@
 //
 // Author:
 //   JCMais
-import { Robot } from 'hubot'
+import { Robot, Response } from 'hubot'
+import SlackAdapter from 'hubot-slack'
 import axios from 'axios'
+import {
+  assertNotPrivateMessage,
+  assertNotInsideThread,
+  workInProgress,
+} from '../utils'
 
-module.exports = async function example(robot: Robot<{}>) {
-  robot.respond(/(http(?:s?):\/\/(\S*))/i, async res => {
-    const url = res.match[1]
-    res.send(`ok1: ${url}`)
+module.exports = async function example(robot: Robot<SlackAdapter>) {
+  robot.respond(
+    /(http(?:s?):\/\/(\S*))/i,
+    async (res: Response<SlackAdapter>) => {
+      const url = res.match[1]
+      res.send(`ok1: ${url}`)
 
-    await axios.get(url)
+      await axios.get(url)
 
-    res.send(`ok2: ${url}`)
+      res.send(`ok2: ${url}`)
+    },
+  )
+
+  robot.respond(/test private$/, async (res: Response<SlackAdapter>) => {
+    await assertNotPrivateMessage(robot, res)
+  })
+
+  robot.respond(/test thread$/, async (res: Response<SlackAdapter>) => {
+    await assertNotInsideThread(robot, res)
+  })
+
+  robot.respond(/test wip$/, async (res: Response<SlackAdapter>) => {
+    await workInProgress(robot, res)
+  })
+
+  robot.respond(/test error$/, async (res: Response<SlackAdapter>) => {
+    throw new Error('Are uncaught errors being sent to channel?')
   })
 }
